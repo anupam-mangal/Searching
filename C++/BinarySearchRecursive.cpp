913fc8ffe7212bff87d58a48d225deddc29c07be
anupam-mangal <44960706+anupam-mangal@users.noreply.github.com>
2021-05-12 10:51:46 +0530

    Create BinarySearchRecursive.cpp

---
+++ C++/BinarySearchRecursive.cpp
@@ -0,0 +1,57 @@
+// Binary Search is done on a sorted array.
+// Time Complexity : O(log n) for searching in 'n' elements.
+// Space Complexity : O(1)
+
+/*
+ * @param arr A vector of integer elements.
+ * @param left A positive integer specifying the leftmost index in the array to consider for the search.
+ * @param right A positive integer specifying the rightmost index in the array to consider for the search.
+ * @param item The integer to be searched.
+ *
+ * @return int Returns the index of the item if found, -1 otherwise.
+ */
+int binary_search_recursive(vector<int> arr, unsigned int start, unsigned int end, int item)
+{
+    if (end < start)
+        return -1;
+
+    int mid = start + (end - start) / 2;
+
+    if (item == arr[mid])
+        return mid;
+
+    else if (item < arr[mid])
+        return binary_search_recursive(arr, start, mid - 1, item);
+        
+    else // item > arr[mid]
+        return binary_search_recursive(arr, mid + 1, end, item);
+}
+
+// Template version of the above algorithm
+
+/*
+ * @param arr A vector of elements.
+ * @param left A positive integer specifying the leftmost index in the array to consider for the search.
+ * @param right A positive integer specifying the rightmost index in the array to consider for the search.
+ * @param item The item to be searched.
+ *
+ * @return int Returns the index of the item if found, -1 otherwise.
+ */
+
+template <typename T>
+int binary_search_recursive(vector<T> arr, unsigned int left, unsigned int right, T item)
+{
+    if (end < start)
+        return -1;
+
+    int mid = start + (end - start) / 2;
+
+    if (item == arr[mid])
+        return mid;
+
+    else if (item < arr[mid])
+        return binary_search_recursive(arr, start, mid - 1, item);
+        
+    else // item > arr[mid]
+        return binary_search_recursive(arr, mid + 1, end, item);
+}
