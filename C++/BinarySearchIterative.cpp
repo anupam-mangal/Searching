f7456398ab09239dd3ac89528d3a28aea183732b
anupam-mangal <44960706+anupam-mangal@users.noreply.github.com>
2021-05-12 10:47:14 +0530

    Update BinarySearchIterative.cpp

--- C++/BinarySearchIterative.cpp
+++ C++/BinarySearchIterative.cpp
@@ -32,3 +32,38 @@ int binary_search_iterative(vector<int> arr, unsigned int left, unsigned int rig
     
     return -1;
 }
+
+// Template version of the above algorithm
+
+/*
+ * @param arr A vector of elements.
+ * @param left A positive integer specifying the leftmost index in the array to consider for the search.
+ * @param right A positive integer specifying the rightmost index in the array to consider for the search.
+ * @param item The item to be searched.
+ *
+ * @return int Returns the index of the item if found, -1 otherwise.
+ */
+
+template <typename T>
+int binary_search_iterative(vector<T> arr, unsigned int left, unsigned int right, T item)
+{
+    // Item should be inside the range of sorted array elements
+    if ((item < arr[left]) || (item > arr[right]))
+        return -1;
+
+    while (left <= right)
+    {
+        int mid = left + (right - left) / 2;
+
+        if (item == arr[mid])
+            return mid;
+
+        else if (item < arr[mid])
+            right = mid - 1;
+        
+        else // item > arr[mid]
+            left = mid + 1;
+    }
+    
+    return -1;
+}
