e3802fa56cfc032b0e1bba926cfcca60298966c4
anupam-mangal <44960706+anupam-mangal@users.noreply.github.com>
2021-05-12 10:36:15 +0530

    Binary Search Iterative

---
+++ C++/BinarySearchIterative.cpp
@@ -0,0 +1,34 @@
+// Binary Search is done on a sorted array.
+// Time Complexity : O(log n) for searching in 'n' elements.
+// Space Complexity : O(1)
+
+/*
+ * @param arr A vector of integer elements.
+ * @param left A positive integer specifying the leftmost index in the array to consider for the search.
+ * @param right A positive integer specifying the rightmost index in the array to consider for the search.
+ * @param item The integer to be searched.
+ *
+ * @return int Returns the index of the item if found, -1 otherwise.
+ */
+int binary_search_iterative(vector<int> arr, unsigned int left, unsigned int right, int item)
+{
+    // Item should be inside the range of sorted array elements
+    if ((item < arr[left]) || (item > arr[right]))
+        return -1;
+
+    while (left <= right)
+    {
+        int mid = left + (right - left) / 2;
+
+        if (item == arr[mid])
+            return mid;
+
+        else if (item < arr[mid])
+            right = mid - 1;
+        
+        else // item > arr[mid]
+            left = mid + 1;
+    }
+    
+    return -1;
+}
